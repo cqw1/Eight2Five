70f4bf720d91d3dd2a9060a41ea8c4c3960aa0e0
jaygarg <garg.jay@gmail.com>
2016-03-12 15:17:05 -0800

    Creating home.js

---
+++ static/js/home.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    console.log('home.js document ready'); 
+
+    // Subscribe Modal
+    $(window).load(function(){
+	  setTimeout(function(){
+	      $('#subscribeModal').modal('show');
+	  }, 5000);
+	});
