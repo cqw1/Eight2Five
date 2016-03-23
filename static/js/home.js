6e3d2646bb1a6ad33091d21ce4dfa61be0cee927
jaygarg <garg.jay@gmail.com>
2016-03-23 15:25:41 -0400

    Update home.js

--- static/js/home.js
+++ static/js/home.js
@@ -1,10 +1,4 @@
 $(document).ready(function() {
     console.log('home.js document ready'); 
-
-    // Subscribe Modal
-    $(window).load(function(){
-	  setTimeout(function(){
-	      $('#subscribeModal').modal('show');
-	  }, 10000);
-	});
+    
 });
